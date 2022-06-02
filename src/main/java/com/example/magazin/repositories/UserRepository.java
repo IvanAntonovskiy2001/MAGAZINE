48fcac807ab528e1fc71bf551b8dd2d6793829e3
Ivan <mr.mr.van@gmail.com>
2022-06-02 19:50:18 +0300

    Revert "new Version 0.0.3 - перестали раюотать некоторые функции"
    This reverts commit c145d16db79f40cba648892fcbb69b6b0eee3a5f.

--- src/main/java/com/example/magazin/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.magazin.repositories;
-
-import com.example.magazin.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
